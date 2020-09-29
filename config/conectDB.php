1a7451882b91d43e878e7c4ffae86840402512b3
Boonyarit Boonkhum <taxboon@hotmail.com>
2020-09-29 14:32:25 +0700

    config server

--- config/conectDB.php
+++ config/conectDB.php
@@ -1,3 +1,7 @@
 <?php
-$dbcon = mysqli_connect('us-cdbr-east-02.cleardb.com', 'b0f75929adfba3', 'd4ffe290', 'heroku_4a36dc84fa25935') or die('disconnect MySQL' . mysqli_connect_error());
+$server = 'us-cdbr-east-02.cleardb.com';
+$username = 'b0f75929adfba3';
+$password = 'd4ffe290';
+$database = 'heroku_4a36dc84fa25935';
+$dbcon = mysqli_connect($server, $username, $password, $database) or die('disconnect MySQL' . mysqli_connect_error());
 mysqli_set_charset($dbcon, 'utf8');
