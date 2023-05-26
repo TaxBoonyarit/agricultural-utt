f393cc6f7428ac2bcabfc96d2378310464b7a440
boonyarit <boonyarit.b@securitypitch.com>
2023-05-26 13:58:14 +0700

    Add docker compose and docker file

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM php:7.4-apache
+
+COPY . /var/www/html
+
+RUN docker-php-ext-install mysqli pdo pdo_mysql
--- config/conectDB.php
+++ config/conectDB.php
@@ -1,7 +1,7 @@
 <?php
-$server = 'us-cdbr-east-02.cleardb.com';
-$username = 'b0f75929adfba3';
-$password = 'd4ffe290';
-$database = 'heroku_4a36dc84fa25935';
+$server = 'db';
+$username = $_ENV["MYSQL_USER"];
+$password = $_ENV["MYSQL_PASSWORD"];
+$database = $_ENV["MYSQL_DATABASE"];
 $dbcon = mysqli_connect($server, $username, $password, $database) or die('disconnect MySQL' . mysqli_connect_error());
 mysqli_set_charset($dbcon, 'utf8');
